c38c026e81916c8b5067979e576bd037d3389f83
Lucas Cuba <lucas.cuba4@gmail.com>
2017-12-10 15:05:45 -0500

    checkpoint 5 commit: added content to library page and improved album routing

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const Album = () => (
-  <section className="Album">
-  </section>
-);
+class Album extends Component {
+  render() {
+    return (
+      <section className='album'>
+        {this.props.match.params.slug} Album will go here
+      </section>
+    );
+  }
+}
 
 export default Album;

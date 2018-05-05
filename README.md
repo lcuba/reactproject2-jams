8c7e70ea947c937a5fd6f833e2884ee254eef00b
Lucas Cuba <lucas.cuba4@gmail.com>
2018-05-05 00:13:10 -0400

    Final deploy for now, updated readme with deploy link

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Welcome to React Jams!
 ### A Music Player React App Created by Lucas Cuba
 
-So Glad you came to check out my music player. This application was built in React and compiled with Webpack. It features a landing page, a library page, and album pages, and since it's React, adding new albums and features is a breeze. 
+So Glad you came to check out my music player. This application was built in React and compiled with Webpack. It features a landing page, a library page, and album pages, and since it's React, adding new albums and features is a breeze.
 
-If you want to see this app deployed, I have it right here:
+If you want to see this app deployed, I have it right here: https://lcuba.github.io/reactproject2-jams/
+Note that I'm not exactly a master of Webpack and how deploying an app like this with many assets is supposed to work, but I tried my best. A lot of the icons are missing.
--- src/data/albums.js
+++ src/data/albums.js
@@ -2,14 +2,14 @@ export default [{
   title: 'Medium Electronic',
   artist: 'Kevin Macleod',
   releaseInfo: '2016-2017 Creative Commons',
-  albumCover: '/assets/images/album_covers/01.jpg',
+  albumCover: './assets/images/album_covers/01.jpg',
   slug: 'medium-electronic',
   songs: [
-      { title: 'Chillin Hard', duration: '234', audioSrc: '/assets/music/Medium/Chillin Hard.mp3' },
-      { title: 'Digital Lemonade', duration: '180', audioSrc: '/assets/music/Medium/Digital Lemonade.mp3' },
-      { title: 'Inspired', duration: '286', audioSrc: '/assets/music/Medium/Inspired.mp3' },
-      { title: 'Jerry Five', duration: '160', audioSrc: '/assets/music/Medium/Jerry Five.mp3' },
-      { title: 'Voice Over Under', duration: '197', audioSrc: '/assets/music/Medium/Voice Over Under.mp3' }
+      { title: 'Chillin Hard', duration: '234', audioSrc: './assets/music/Medium/Chillin Hard.mp3' },
+      { title: 'Digital Lemonade', duration: '180', audioSrc: './assets/music/Medium/Digital Lemonade.mp3' },
+      { title: 'Inspired', duration: '286', audioSrc: './assets/music/Medium/Inspired.mp3' },
+      { title: 'Jerry Five', duration: '160', audioSrc: './assets/music/Medium/Jerry Five.mp3' },
+      { title: 'Voice Over Under', duration: '197', audioSrc: './assets/music/Medium/Voice Over Under.mp3' }
   ]
 }, {
     title: 'Hard Electronic',
@@ -18,10 +18,10 @@ export default [{
     albumCover: '/assets/images/album_covers/02.jpg',
     slug: 'hard-electronic',
     songs: [
-      { title: 'Broken Reality', duration: '218', audioSrc: '/assets/music/Hard/Broken Reality.mp3' },
-      { title: 'Exit the Premises', duration: '210', audioSrc: '/assets/music/Hard/Exit the Premises.mp3' },
-      { title: 'Furious Freak', duration: '187', audioSrc: '/assets/music/Hard/Furious Freak.mp3' },
-      { title: 'Ice Flow', duration: '142', audioSrc: '/assets/music/Hard/Ice Flow.mp3' },
-      { title: 'Severe Tire Damage', duration: '411', audioSrc: '/assets/music/Hard/Severe Tire Damage.mp3' }
+      { title: 'Broken Reality', duration: '218', audioSrc: './assets/music/Hard/Broken Reality.mp3' },
+      { title: 'Exit the Premises', duration: '210', audioSrc: './assets/music/Hard/Exit the Premises.mp3' },
+      { title: 'Furious Freak', duration: '187', audioSrc: './assets/music/Hard/Furious Freak.mp3' },
+      { title: 'Ice Flow', duration: '142', audioSrc: './assets/music/Hard/Ice Flow.mp3' },
+      { title: 'Severe Tire Damage', duration: '411', audioSrc: './assets/music/Hard/Severe Tire Damage.mp3' }
     ]
 }];
